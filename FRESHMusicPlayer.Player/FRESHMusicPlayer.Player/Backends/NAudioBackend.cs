40148a8bde8d657054b8b5234412c493123f8bcd
Royce551 <poohwie2005@gmail.com>
2021-08-21 18:24:01 -0500

    update NAudioBackend

--- FRESHMusicPlayer.Player/FRESHMusicPlayer.Player/Backends/NAudioBackend.cs
+++ FRESHMusicPlayer.Player/FRESHMusicPlayer.Player/Backends/NAudioBackend.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Composition;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace FRESHMusicPlayer.Backends
     [Export(typeof(IAudioBackend))]
     class NAudioBackend : IAudioBackend
     {
-        private readonly WaveOutEvent outputDevice;
+        public WaveOutEvent OutputDevice;
 
         public event EventHandler<EventArgs> OnPlaybackStopped;
 
@@ -27,44 +28,70 @@ namespace FRESHMusicPlayer.Backends
             get => AudioFile.CurrentTime;
             set => AudioFile.CurrentTime = value;
         }
-        public TimeSpan TotalTime => AudioFile.TotalTime; 
+        public TimeSpan TotalTime => AudioFile.TotalTime;
+
+        public IMetadataProvider Metadata { get; private set; }
 
         public NAudioBackend()
         {
-            if (outputDevice is null)
+            if (OutputDevice is null)
             {
-                outputDevice = new WaveOutEvent();
-                outputDevice.PlaybackStopped += (object o, StoppedEventArgs e) =>
+                OutputDevice = new WaveOutEvent();
+                OutputDevice.PlaybackStopped += (object o, StoppedEventArgs e) =>
                 {
-                    OnPlaybackStopped(this, new EventArgs());
+                    OnPlaybackStopped.Invoke(null, EventArgs.Empty);
                 };
             }
         }
 
-        public void LoadSong(string file)
+        public async Task<BackendLoadResult> LoadSongAsync(string file)
         {
-            if (AudioFile != null)
+            if (!File.Exists(file)) return BackendLoadResult.Invalid;
+
+            if (AudioFile != null) AudioFile.Dispose();
+            try
             {
-                AudioFile.Dispose();
+                await Task.Run(() =>
+                {
+                    AudioFile = new AudioFileReader(file);
+                    OutputDevice.Init(AudioFile);
+
+                    Metadata = new FileMetadataProvider(file);
+                });
             }
-            AudioFile = new AudioFileReader(file);
-            outputDevice.Init(AudioFile);
+            catch (ArgumentException)
+            {
+                return BackendLoadResult.Invalid;
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                return BackendLoadResult.Invalid;
+            }
+            catch (FormatException)
+            {
+                return BackendLoadResult.Corrupt;
+            }
+            catch (InvalidOperationException)
+            {
+                return BackendLoadResult.Invalid;
+            }
+            return BackendLoadResult.OK;
         }
 
         public void Play()
         {
-            outputDevice.Play();
+            OutputDevice.Play();
         }
 
         public void Dispose()
         {
-            outputDevice.Dispose();
+            OutputDevice.Dispose();
             AudioFile.Dispose();
         }
 
         public void Pause()
         {
-            outputDevice.Pause();
+            OutputDevice.Pause();
         }
     }
 }
