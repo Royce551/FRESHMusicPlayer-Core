67daf6932812e2bab8950b17c7279b6d5c834615
Royce551 <poohwie2005@gmail.com>
2021-08-21 18:13:58 -0500

    update IAudioBackend

---
+++ FRESHMusicPlayer.Player/FRESHMusicPlayer.Player/Backends/FileMetadataProvider.cs
@@ -0,0 +1,34 @@
+﻿using ATL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRESHMusicPlayer.Backends
+{
+    public class FileMetadataProvider : IMetadataProvider
+    {
+        public string Title => ATLTrack.Title;
+                                                                                                    // for mp3s, ATL still uses /
+        public string[] Artists => ATLTrack.Artist.Split(new char[] { Settings.DisplayValueSeparator, '/' });
+
+        public string Album => ATLTrack.Album;
+
+        public byte[] CoverArt => ATLTrack.EmbeddedPictures[0].PictureData;
+
+        public string[] Genres => ATLTrack.Genre.Split(new char[] { Settings.DisplayValueSeparator, '/' });
+
+        public int Year => ATLTrack.Year;
+
+        public int TrackNumber => ATLTrack.TrackNumber;
+
+        public int TrackTotal => ATLTrack.TrackTotal;
+
+        public int DiscNumber => ATLTrack.DiscNumber;
+
+        public int DiscTotal => ATLTrack.DiscTotal;
+
+        public Track ATLTrack { get; set; }
+
+        public FileMetadataProvider(string path) => ATLTrack = new Track(path);
+    }
+}
--- FRESHMusicPlayer.Player/FRESHMusicPlayer.Player/Backends/IAudioBackend.cs
+++ FRESHMusicPlayer.Player/FRESHMusicPlayer.Player/Backends/IAudioBackend.cs
@@ -8,15 +8,39 @@ namespace FRESHMusicPlayer.Backends
 {
     public interface IAudioBackend : IDisposable
     {
-        void LoadSong(string file);
+        Task<BackendLoadResult> LoadSongAsync(string file);
         void Play();
         void Pause();
 
         TimeSpan CurrentTime { get; set; }
         TimeSpan TotalTime { get; }
 
+        IMetadataProvider Metadata { get; }
+
         float Volume { get; set; }
 
         event EventHandler<EventArgs> OnPlaybackStopped;
+    }
+
+    public interface IMetadataProvider
+    {
+        string Title { get; }
+        string[] Artists { get; }
+        string Album { get; }
+        byte[] CoverArt { get; }
+        string[] Genres { get; }
+        int Year { get; }
+        int TrackNumber { get; }
+        int TrackTotal { get; }
+        int DiscNumber { get; }
+        int DiscTotal { get; }
+    }
+
+    public enum BackendLoadResult
+    {
+        OK,
+        NotSupported,
+        Invalid,
+        Corrupt
     }
 }
